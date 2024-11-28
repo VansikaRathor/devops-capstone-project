2cc02ff2ff3de874297cb1d8e77ace0218706060
VansikaRathor <rathorvansika@gmail.com>
2024-11-28 05:44:34 -0500

    Added security headers

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -5,6 +5,7 @@ Test cases can be run with the following:
   nosetests -v --with-spec --spec-color
   coverage report -m
 """
+from service import talisman
 import os
 import logging
 from unittest import TestCase
@@ -18,6 +19,8 @@ DATABASE_URI = os.getenv(
 )
 
 BASE_URL = "/accounts"
+
+HTTPS_ENVIRON = {'wsgi.url_scheme': 'https'}
 
 
 ######################################################################
@@ -34,6 +37,12 @@ class TestAccountService(TestCase):
         app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
         app.logger.setLevel(logging.CRITICAL)
         init_db(app)
+
+    @classmethod
+    def setUpClass(cls):
+        """Run once before all tests"""
+        { other lines of code here ... }
+        talisman.force_https = False
 
     @classmethod
     def tearDownClass(cls):
@@ -160,3 +169,16 @@ class TestAccountService(TestCase):
         """It should not allow an illegal method call"""
         resp = self.client.delete(BASE_URL)
         self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def test_security_headers(self):
+        """It should return security headers"""
+        response = self.client.get('/', environ_overrides=HTTPS_ENVIRON)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        headers = {
+            'X-Frame-Options': 'SAMEORIGIN',
+            'X-Content-Type-Options': 'nosniff',
+            'Content-Security-Policy': 'default-src \'self\'; object-src \'none\'',
+            'Referrer-Policy': 'strict-origin-when-cross-origin'
+        }
+        for key, value in headers.items():
+            self.assertEqual(response.headers.get(key), value)
